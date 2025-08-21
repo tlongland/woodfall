f25b26e0c58793303286e271379c07ff9e604549
tlongland <tlongland123@gmail.com>
2025-08-21 21:01:00 +0100

    Create gulpfile.js

---
+++ wp-content/themes/new-theme/gulpfile.js
@@ -0,0 +1,68 @@
+const project = 'woodfall';
+
+const {src, dest, watch, series, parallel} = require('gulp');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+const concat = require('gulp-concat');
+const gsass = require('gulp-dart-sass');
+
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
+
+function watcher(cb) {
+    browserSync.init({
+        proxy: {
+            target: 'http://localhost:8888/' + project + '/'
+        }
+    });
+
+    watch(
+        '/.dev/js/**/*.js',
+        series(scripts, browserReload)
+    );
+
+    watch(
+        '.dev/sass/**/*.scss',
+        series(styles)
+    );
+
+    watch('./**/*.php', series(browserReload));
+}
+
+function browserReload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+function scripts(cb) {
+    return src([
+        './dev/js/app.js'
+    ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(dest('./scripts/'))
+}
+
+function styles() {
+    return src('.dev/sass/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(gsass({
+        outputStyle: 'compressed',
+        quietDeps: true,
+    }).on('error', gsass.logError))
+    .pipe(autoprefixer())
+    .pipe(clean({debug:true}))
+    .pipe(sourcemaps.write())
+    .pipe(dest('.'))
+    .pipe(browserSync.stream())
+}
+
+exports.scripts = scripts;
+exports.styles = styles;
+
+exports.default = series(watcher);
